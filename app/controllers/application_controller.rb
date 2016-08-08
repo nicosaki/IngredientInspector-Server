0ecdf7b39869383343191ddcdd4b766479ccd4d9
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-08-08 15:50:50 -0700

    helper method in app controller to clean ingredient input fetched by upc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :clean_input
+
+  def clean_input(string)
+    string = string.gsub(/[()\[\]]/, ",")
+    ingredients = string.split(",")
+    puts "HERE"
+    puts ingredients
+    ingredients.each do |ingredient|
+      ingredient.slice!(0) if (ingredient[0] == " ")
+      ingredient.slice!(-1) if (ingredient[-1] == " ")
+      ingredients.delete("")
+      ingredients.delete(". ")
+    end
+    return ingredients
+  end
+
 end
