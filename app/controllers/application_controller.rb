be8e663dd60f1bffcc2951d5f659594270f1566d
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-08-11 11:54:41 -0700

    Added cacheing to manufacturer API lookups => contact infos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,21 +19,38 @@ class ApplicationController < ActionController::Base
   end
 
   def fetch_manufacturer(brand)
-    response = HTTParty.get("https://api.cognitive.microsoft.com/bing/v5.0/search?q=#{brand}&count=3&offset=0&mkt=en-us&safesearch=Strict",
-      headers: { "Accept" => "application/json",
-        "User-Agent" => "IngredientInspector/1.0",
-        "Ocp-Apim-Subscription-Key" => ENV['BING_SEARCH_KEY_1'],
-        "Authorization" => ENV['BING_SEARCH_KEY_2']})
-    url = response["webPages"]["value"][0]["displayUrl"]
+    url = Rails.cache.fetch("#{brand}/brand")
+    if url.nil?
+      response = HTTParty.get("https://api.cognitive.microsoft.com/bing/v5.0/search?q=#{brand}&count=3&offset=0&mkt=en-us&safesearch=Strict",
+        headers: { "Accept" => "application/json",
+          "User-Agent" => "IngredientInspector/1.0",
+          "Ocp-Apim-Subscription-Key" => ENV['BING_SEARCH_KEY_1'],
+          "Authorization" => ENV['BING_SEARCH_KEY_2']})
+      url = response["webPages"]["value"][0]["displayUrl"]
+      puts "USED A BING QUERY"
+      Rails.cache.fetch("#{brand}/brand", expires_in: 200.hours) do
+        url
+      end
+    end
     return url
   end
 
   def fetch_contact(domain)
-    response = HTTParty.get("https://api.fullcontact.com/v2/company/lookup.json?domain=#{domain}",
-      headers: { "Accept" => "application/json",
-        "User-Agent" => "IngredientInspector/1.0",
-        "X-FullContact-APIKey" => ENV['FULL_CONTACT_KEY']
-        })
-    return response
+    contact = Rails.cache.fetch("#{domain}/domain")
+    # puts contact
+    if contact.nil?
+      contact = HTTParty.get("https://api.fullcontact.com/v2/company/lookup.json?domain=#{domain}",
+        headers: { "Accept" => "application/json",
+          "User-Agent" => "IngredientInspector/1.0",
+          "X-FullContact-APIKey" => ENV['FULL_CONTACT_KEY']
+          })["socialProfiles"]
+      puts "USED A FULLCONTACT QUERY"
+      Rails.cache.fetch("#{domain}/domain", expires_in: 200.hours) do
+        contact
+      end
+      puts contact
+    end
+    return contact
   end
+
 end
