74aedb7965c8761ff70643d5ef6c768e9b83030b
Nicole Iwasaki <Nicole.A.Iwasaki@gmail.com>
2016-08-11 12:01:42 -0700

    Added cacheing to overarching ingredient searching function and altered return/cached  data structure

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -5,51 +5,66 @@ class IngredientsController < ApplicationController
   def lookup
     upc = params[:upc]
     id = params[:id]
-    response = HTTParty.get("http://world.openfoodfacts.org/api/v9/product/#{upc}.json",
-      headers: { "Accept" => "application/json",
-        "User-Agent" => "IngredientInspector/1.0"})
-    puts response
-    if (response.parsed_response["status"]==0)
-      render json: response.parsed_response["status_verbose"].as_json
-    elsif (response.parsed_response["product"]["ingredients_text"] == "")
-      #code to search secondary food database!!!
-      product = response.parsed_response["product"]["product_name"]
-      brand = response.parsed_response["product"]["brands"]
-      data = { product: product, brand: brand, message: "No ingredients added"}
-      render json: data.as_json
-    else
-      all_ingredients = response.parsed_response["product"]["ingredients_text"]
-      ingredients = clean_input(all_ingredients)
-      #code to run all of the queries, plus manufacturer API
-      data = {
-            "all" => all_ingredients,
-            "ingredients" => "array of hashes?",
-            "manufacturer_contact" => "email_goes_here",
-            "a" => ingredients
+    #code to fetch from cache here
+    data = Rails.cache.fetch("#{upc}/upc")
+    if data.nil?
+      response = HTTParty.get("http://world.openfoodfacts.org/api/v9/product/#{upc}.json",
+        headers: { "Accept" => "application/json",
+          "User-Agent" => "IngredientInspector/1.0"})
+      puts response
 
+      if (response.parsed_response["status"]==0)
+        data = response.parsed_response["status_verbose"]
+      elsif (response.parsed_response["product"]["ingredients_text"] == "")
+        #code to search secondary food database!!!
+        data = { product: product, brand: brand, message: "No ingredients added"}
+      else
+        all_ingredients = response.parsed_response["product"]["ingredients_text"]
+        ingredients = clean_input(all_ingredients)
+        product = response.parsed_response["product"]["product_name"]
+        brand = response.parsed_response["product"]["brands"]
+        contact = fetch_contact(fetch_manufacturer(brand))
+        domain = fetch_manufacturer(brand)
+      # async script to get manufacturer domain => contact info
+      #code to run all of the db queries locally
+      data = {
+            "ingredients" => ingredients,
+            "manufacturer_contact" => contact,
+            "product" => product,
+            "brand" => brand,
+            "domain" => domain
           }
+      end
+      Rails.cache.fetch("#{upc}/upc", expires_in: 200.hours) do
+        data
+      end
+    end
 
       render json: data.as_json, :status => :ok
 
-    end
-        # render json: [], :status => :bad_request
-
+      # render json: [], :status => :bad_request
   end
 
   def test
-    # array = ["carrageenan", "Gluconic acid", "starch", "caramel color"]
-    # hold = []
+    # array = ["corn syrup", "modified food starch", "citric acid", "malic acid", "fumaric acid", "dextrin", "carnauba wax", "triglycerides", "tartrazine"]
+    hold = []
     # array.each do |ingredient|
     #   this = Ingredient.find_by(name: ingredient)
     #   puts this
     #   if this
     #     hold << ingredient
+    #     puts this.status
+    #     puts this.warnings
     #   end
     # end
     # details = Ingredient.where(status: nil, warnings: nil)
-    details = fetch_contact("http://misttwst.com/")
+    # details = fetch_contact("http://misttwst.com/")
     # puts details.length
-    render json: details
+    ingredients = Ingredient.all
+    ingredients.each do |ingredient|
+      hold << [ingredient.name, ingredient.status, ingredient.warnings]
+    end
+    render json: hold.as_json
   end
 
 end
